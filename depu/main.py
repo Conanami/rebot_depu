ff032316d14042765bc99cbde6064c1c1bd163c4
huweiping <huwp001@qq.com>
2018-09-05 15:45:34 +0800

    首次完成机器人整合。

--- depu/main.py
+++ depu/main.py
@@ -5,8 +5,6 @@ from PIL import Image
 import pyautogui
 import win32api
 import win32gui
-#import win32ui
-#from ctypes import windll
 import win32con
 import numpy
 import time
@@ -15,8 +13,9 @@ import logging
 from threading import Thread
 from pynput import keyboard
 from pynput.keyboard import Listener
-from multiprocessing import Process, Queue, Pool
-from readpot import readimg
+from read_pc_depu import analysisImg
+from multiprocessing import Queue
+import player as p
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')  # logging.basicConfig函数对日志的输出格式及方式做相关配置
@@ -85,6 +84,29 @@ def run_monitor_key(q):
     with Listener(on_release=on_release) as listener:
         listener.join()
 
+def handle(game_area_left, game_area_top, rtSit):
+    '''  处理结果 '''
+    target = 0, 0
+    logging.info('开始决策')
+    kind, no = p.makeDecision(rtSit)
+    logging.info('完成决策， 结果 %s %s' %(kind, no))
+    if kind==0:
+        #弃牌
+        target = 290, 550
+    elif kind == 1 or kind==2:
+        # 追加
+        target = 450, 550
+    elif kind ==3:
+        if no==1:
+            target = 590, 550
+        elif no==2:
+            target = 700, 550
+        elif no==3:
+            target = 800, 550
+    if target[0]:
+        pyautogui.moveTo(game_area_left+target[0],game_area_top+target[1])
+        pyautogui.click()
+
 @async
 def run_game(q):
     lastkey = keyboard.Key.esc
@@ -104,8 +126,11 @@ def run_game(q):
             if game_area_left:
                 # pyautogui.moveTo(game_area_left,game_area_top)
                 game_area_image = get_game_data(game_area_left, game_area_top, game_area_width, game_area_height)
-                game_area_image.save(r'depu/0903/dz_%s.png' % time.strftime("%d%H%M%S", time.localtime()))
-                readimg(game_area_image.convert('L'))
+                game_area_image.save(r'tmp/%s/dz_%s.png' % (time.strftime("%m%d", time.localtime()), time.strftime("%H%M%S", time.localtime())))
+                logging.info('开始解析图像')
+                rt = analysisImg(game_area_image.convert('L'))
+                logging.info('完成解析图像')
+                handle(game_area_left, game_area_top, rt)
         else:
             lastkey = keyboard.Key.esc
             pass
