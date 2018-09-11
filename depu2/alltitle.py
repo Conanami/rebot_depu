cf6dd3daffab063014fddb16a5f65e949cd835dc
huweiping <huwp001@qq.com>
2018-09-12 06:37:06 +0800

    开发 德扑2.0版本

---
+++ depu2/alltitle.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from win32gui import *
+titles = set()
+def foo(hwnd,mouse):
+    #去掉下面这句就所有都输出了，但是我不需要那么多
+    if IsWindow(hwnd) and IsWindowEnabled(hwnd) and IsWindowVisible(hwnd):
+        titles.add(GetWindowText(hwnd))
+
+def findTitle(title):
+    EnumWindows(foo, 0)
+    lt = [t for t in titles if t]
+    lt.sort()
+    for t in lt:
+        if title in t:
+            return t
+    return None
+
+# r = findTitle('大厅')
+# print(r)
