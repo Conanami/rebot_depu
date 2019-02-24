e079c4bfb8ef0584762b37a663633b9adc8e27bf
shinefool@hotmail.com <shinefool@hotmail.com>
2019-02-24 10:27:29 +0800

    BUGFIX：测试一下永远偷

--- depu2/test.py
+++ depu2/test.py
@@ -15,14 +15,16 @@ from reader import LastManAfterFlop
 from preFlop import IsOpenRange
 from preFlop import IsFirst
 from handRange import RankHand
+from handRange import InRangeHand
 from reader import getMyHand
 from dealer import card
+
 
 def bbb():
     ''' 测试 '''
     start =time.clock()
    
-   
+    #这段是最主要的测试
     file_name=r'tmp\dz_0217060521.png' 
     wholeimg=Image.open(file_name).convert('L')
     #要解析的图片，和后面一堆样本图片
@@ -30,14 +32,16 @@ def bbb():
     rtSit=analysisImg(wholeimg,0.02)
     
     #入库
-    print(rtSit.todict())
+    #print(rtSit.todict())
     
     #判断在我之前的人是否都弃牌
-    print(IsFirst(rtSit))
-    print(IsOpenRange(rtSit))
-    
-    myhand=[card(2,14),card(2,2)]
+    print('是不是第一个入池',IsFirst(rtSit))
+    print('范围内',IsOpenRange(rtSit))
+    print('决策结果%s %s' % (p.makeDecision(rtSit)))
+
+    myhand=[card(3,6),card(2,8)]
     print(RankHand(myhand))
+    print(InRangeHand(myhand,51))
     #print(MyTurn(rtSit))
     
     #winrate=calcuWinrateNoWeight(rtSit)
@@ -45,24 +49,8 @@ def bbb():
     #print('胜率'+str(winrate[0])+','+str(winrate[1]))
     #print('不喜欢率%s , %s' % DontLikeRate(rtSit,winrate))
     #print('翻后最后一个行动的人的位置:'+str(LastManAfterFlop(rtSit)))
-    print('决策结果%s %s' % (p.makeDecision(rtSit)))
-    '''
-    file_name=r'tmp\dz_0924103508.png' 
-    wholeimg=Image.open(file_name).convert('L')
-    #要解析的图片，和后面一堆样本图片
-    #print(NeedAnalyse(wholeimg))
-    rtSit2=analysisImg(wholeimg,0.02)
+    #print('决策结果%s %s' % (p.makeDecision(rtSit)))
     
-    print(rtSit2.todict())
-    #print(getWaitingman(rtSit))
-    print(IsSameHand(rtSit,rtSit2))
-    
-    #
-    #print('决策结果%s %s' % (p.makeTmpDecision(rtSit)))
-    #for i in range(15):
-    #    print(random.random())
-    #计算运行时间
-    '''
     end = time.clock()
     print('Running time: %s Seconds'%(end-start)) 
 
